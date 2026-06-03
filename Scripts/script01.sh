3eb728b2a457de8233f61922db775d35488fbf4e
RAICHAND <shreyash.raichand@capgemini.com>
2026-06-03 15:47:50 +0530

    Multi Jobs

--- Scripts/script01.sh
+++ Scripts/script01.sh
@@ -1,3 +1,2 @@
-touch file01.txt
-echo "THis is an example file" > file01.txt
+echo "This is the line added in Job no.1 at ${date}" >> file01.txt
 chmod 604 file01.txt
---
+++ Scripts/script02.sh
@@ -0,0 +1,2 @@
+echo "This is the line added in Job no.2 at ${date}" >> file01.txt
+chmod 700 file01.txt
