6e322166f9cedfad6ea7da6af43970af76f8615a
RAICHAND <shreyash.raichand@capgemini.com>
2026-06-06 16:27:52 +0530

    Outputs - correcting error

--- Scripts/script02.sh
+++ Scripts/script02.sh
@@ -1,3 +1,3 @@
 echo "This is the line added in Job no.2" >> file01.txt
-echo "This is the output generated in job no. 1 --> ${{ needs.Script-1.outputs.statement }} >> file01.txt
+echo "This is the output generated in job no. 1 --> ${{ needs.Script-1.outputs.statement }} >> file01.txt"
 chmod 700 file01.txt
