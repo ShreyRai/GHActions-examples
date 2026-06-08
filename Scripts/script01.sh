e6504b8634cf5c29990bde31b7ecb5475492e63d
RAICHAND <shreyash.raichand@capgemini.com>
2026-06-08 16:09:19 +0530

    Tesing contexts

--- Scripts/script01.sh
+++
@@ -1,2 +0,0 @@
-echo "This is the line added in Job no.1 at ${date}" >> file01.txt
-chmod 604 file01.txt
--- Scripts/script02.sh
+++
@@ -1,3 +0,0 @@
-echo "This is the line added in Job no.2" >> file01.txt
-echo "This is the output generated in job no. 1 --> ${MY_VAR}" >> file01.txt
-chmod 700 file01.txt
